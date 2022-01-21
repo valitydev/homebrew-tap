10c6d27875bcd7a3aab8fe29e360577c386c2580
Pavel Popov <popoffpavel@yandex.ru>
2022-01-21 16:15:14 +0300

    Bump woorl to 1.6, switch prebuild binaries, add Monterey instead Linux build

--- Formula/woorl.rb
+++ Formula/woorl.rb
@@ -1,24 +1,20 @@
 class Woorl < Formula
   desc "Woody curl"
   homepage "https://github.com/valitydev/woorl/"
-  url "https://github.com/valitydev/woorl.git",
-      tag:      "1.5",
-      revision: "324c4dd2dc21f01200a5302d58b8d9424e74814d"
+  url "https://github.com/valitydev/woorl/releases/download/1.6/woorl-1.6.tar.gz"
+  sha256 "f2dd57c34db4a20b9e8c970f0b17c300f7969d48ec4f3e2263bc7e8ea6f649d4"
   license "Apache-2.0"
-  head "https://github.com/valitydev/woorl.git", branch: "master"
 
   bottle do
     root_url "https://github.com/valitydev/homebrew-tap/releases/download/woorl-1.5"
     sha256 cellar: :any_skip_relocation, all: "48217f4da7005b560c83f96b1a664f27e869a5360b50ac4c3a494f3d2cc89808"
   end
 
-  depends_on "rebar3" => :build
   depends_on "erlang"
-  depends_on "vality-thrift" => :optional
+  depends_on "vality-thrift"
 
   def install
-    system "rebar3", "escriptize"
-    bin.install "_build/default/bin/woorl" => "woorl"
+    bin.install "woorl"
   end
 
   test do
