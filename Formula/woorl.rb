9f35b2eec4267a8f2bcae9d6d62371485b5ea82c
Pavel Popov <5084395+tolkonepiu@users.noreply.github.com>
2022-01-21 04:02:48 +0000

    woorl: add 1.5 bottle.

--- Formula/woorl.rb
+++ Formula/woorl.rb
@@ -5,6 +5,12 @@ class Woorl < Formula
   sha256 "e6caf0194111ee239e6d305786778ae948b1411849eb09559e941774b417179e"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/valitydev/homebrew-tap/releases/download/woorl-1.5"
+    sha256 cellar: :any_skip_relocation, big_sur:      "163b828c5716a4e212993e3f400f5df8a99c4832935f01d4bebcbe5c240717b4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a6b609dec8a3789f82bb5e6f00ad18a926956ea62860f4b6d20a5f6e6c53cbf3"
+  end
+
   depends_on "rebar3" => :build
   depends_on "erlang"
 
