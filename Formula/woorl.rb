45bac197eb11dbe6479ada56d73f9cef332c44a9
Anatoly Karlov <karleowne@gmail.com>
2024-06-20 19:29:57 +0700

    Bump vality woorl to 1.9

--- Formula/woorl.rb
+++ Formula/woorl.rb
@@ -1,24 +1,20 @@
 class Woorl < Formula
   desc "Woody curl"
   homepage "https://github.com/valitydev/woorl/"
-  url "https://github.com/valitydev/woorl.git",
-      tag:      "1.9",
-      revision: "d7b3e5c959b693b166950bbec78e1c4ed7a10285"
+  url "https://github.com/valitydev/woorl/releases/download/1.9/woorl-1.9.tar.gz"
+  sha256 "7fb978afc44798ee85bd87e8f30241b8a027754ec8caeaf3c8d686540a4e7be7"
   license "Apache-2.0"
-  head "https://github.com/valitydev/woorl.git", branch: "master"
 
   bottle do
     root_url "https://github.com/valitydev/homebrew-tap/releases/download/woorl-1.5"
     sha256 cellar: :any_skip_relocation, all: "48217f4da7005b560c83f96b1a664f27e869a5360b50ac4c3a494f3d2cc89808"
   end
 
-  depends_on "rebar3" => :build
   depends_on "erlang"
-  depends_on "vality-thrift" => :optional
+  depends_on "vality-thrift"
 
   def install
-    system "rebar3", "escriptize"
-    bin.install "_build/default/bin/woorl" => "woorl"
+    bin.install "woorl"
   end
 
   test do
