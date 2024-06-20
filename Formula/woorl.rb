78cda17700b1b3c6c7477a6774b589c16beb932a
Anatoly Karlov <karleowne@gmail.com>
2024-06-20 20:42:09 +0700

    Bump vality woorl to 1.9, thrift 19

--- Formula/woorl.rb
+++ Formula/woorl.rb
@@ -2,14 +2,9 @@ class Woorl < Formula
   desc "Woody curl"
   homepage "https://github.com/valitydev/woorl/"
   url "https://github.com/valitydev/woorl/archive/refs/tags/1.9.tar.gz"
-  sha256 "7fb978afc44798ee85bd87e8f30241b8a027754ec8caeaf3c8d686540a4e7be7"
+  sha256 "9b7eb046857584ebc2125044bb0224875826229f86c7733714948e7ab8e3c8fc"
   license "Apache-2.0"
   head "https://github.com/valitydev/woorl.git", branch: "master"
-
-  bottle do
-    root_url "https://github.com/valitydev/homebrew-tap/releases/download/woorl-1.5"
-    sha256 cellar: :any_skip_relocation, all: "48217f4da7005b560c83f96b1a664f27e869a5360b50ac4c3a494f3d2cc89808"
-  end
 
   depends_on "erlang"
   depends_on "vality-thrift"
