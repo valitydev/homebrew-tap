380266b66e6c8ef6c285f3961ce27e7cfbd833f2
Anatoly Karlov <karleowne@gmail.com>
2024-06-21 12:40:08 +0700

    fixing bottle update

--- Formula/vality-thrift.rb
+++ Formula/vality-thrift.rb
@@ -1,15 +1,16 @@
 class ValityThrift < Formula
   desc "Vality version of Apache Thrift"
   homepage "https://github.com/valitydev/thrift/"
-  url "https://github.com/valitydev/thrift/archive/0.14.2.tar.gz"
-  sha256 "01d38bab2b49bf2272ef449379b760e1b3bc7d1f1a7e2d2a99f802e2acb0aa76"
+  url "https://github.com/valitydev/thrift/archive/refs/tags/0.17.0.tar.gz"
+  sha256 "9524405c7e0b977c264fca5043b7816e60e8a92d70f7529cb9107a17bc42b1e7"
   license "Apache-2.0"
+  head "https://github.com/valitydev/thrift.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/valitydev/homebrew-tap/releases/download/vality-thrift-0.14.2"
-    sha256 cellar: :any,                 arm64_monterey: "ae4f0e672d001aa86796b02177804be83a908fca4643dab76d5823497d446957"
-    sha256 cellar: :any,                 big_sur:        "41473eeb1efb737c1e07f651dd38a46e23fd32998f3cd2ac4759260b190e513c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f78f17a998f0276f63abda706aa067daabe9eadfd5cdbfebbb415aa6eafe00b5"
+    root_url "https://github.com/valitydev/homebrew-tap/releases/download/vality-thrift-0.17.0"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_sonoma: "a390600db7d197d9e2c3d3bfabbd181fd1b47f9c3bfd15038aa2313962a70210"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d8c2119f05e68a26f4abad3a9791a5fcb2ddc60b2c6f56bc4354e532f4185ab6"
   end
 
   depends_on "autoconf" => :build
@@ -19,6 +20,7 @@ class ValityThrift < Formula
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "openssl@1.1"
+  uses_from_macos "zlib"
 
   conflicts_with "thrift", because: "also installs a 'thrift' executable"
 
@@ -36,13 +38,19 @@ class ValityThrift < Formula
       --without-go
       --without-haskell
       --without-java
+      --without-kotlin
+      --without-python
+      --without-py3
+      --without-haxe
       --without-netstd
-      --without-nodejs
       --without-perl
       --without-php
       --without-php_extension
-      --without-py3
-      --without-python
+      --without-dart
+      --without-d
+      --without-nodejs
+      --without-nodets
+      --without-lua
       --without-rs
       --without-ruby
       --without-swift
@@ -62,7 +70,7 @@ class ValityThrift < Formula
   end
 
   test do
-    (testpath/"test.thrift").write <<~'EOS'
+    (testpath/"test.thrift").write <<~EOS
       service MultiplicationService {
         i32 multiply(1:i32 x, 2:i32 y),
       }
--- Formula/woorl.rb
+++ Formula/woorl.rb
@@ -1,15 +1,16 @@
 class Woorl < Formula
   desc "Woody curl"
   homepage "https://github.com/valitydev/woorl/"
-  url "https://github.com/valitydev/woorl.git",
-      tag:      "1.5",
-      revision: "324c4dd2dc21f01200a5302d58b8d9424e74814d"
+  url "https://github.com/valitydev/woorl/archive/refs/tags/1.8.tar.gz"
+  sha256 "0b2f7e73f119681fe65d04016acd0986da5ddee6a729416baa70828673a935a0"
   license "Apache-2.0"
   head "https://github.com/valitydev/woorl.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/valitydev/homebrew-tap/releases/download/woorl-1.5"
-    sha256 cellar: :any_skip_relocation, all: "48217f4da7005b560c83f96b1a664f27e869a5360b50ac4c3a494f3d2cc89808"
+    root_url "https://github.com/valitydev/homebrew-tap/releases/download/woorl-1.8"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "80064e74b735b5674905d08fc9bfeb05c420d5cf874ddba5f9c730d64b3e61dd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "808b432aaf938dffec2acf4a95d23ec8d1ef53374d22f2f5013b00110b13c022"
   end
 
   depends_on "rebar3" => :build
