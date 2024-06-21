0c4feba0edc0325a03539314075e5f840026d786
Anatoly Karlov <karleowne@gmail.com>
2024-06-21 12:56:01 +0700

    fixing bottle update
    Closes #22.
    
    Signed-off-by: Anatolii Karlov <19729841+karle0wne@users.noreply.github.com>

--- Formula/woorl.rb
+++ Formula/woorl.rb
@@ -1,13 +1,13 @@
 class Woorl < Formula
   desc "Woody curl"
   homepage "https://github.com/valitydev/woorl/"
-  url "https://github.com/valitydev/woorl/archive/refs/tags/1.8.tar.gz"
-  sha256 "0b2f7e73f119681fe65d04016acd0986da5ddee6a729416baa70828673a935a0"
+  url "https://github.com/valitydev/woorl/archive/refs/tags/1.9.tar.gz"
+  sha256 "9b7eb046857584ebc2125044bb0224875826229f86c7733714948e7ab8e3c8fc"
   license "Apache-2.0"
   head "https://github.com/valitydev/woorl.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/valitydev/homebrew-tap/releases/download/woorl-1.8"
+    root_url "https://github.com/valitydev/homebrew-tap/releases/download/woorl-1.9"
     rebuild 1
     sha256 cellar: :any_skip_relocation, arm64_sonoma: "80064e74b735b5674905d08fc9bfeb05c420d5cf874ddba5f9c730d64b3e61dd"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "808b432aaf938dffec2acf4a95d23ec8d1ef53374d22f2f5013b00110b13c022"
